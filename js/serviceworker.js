87fa3c3126b016358cfd9741d71d0d49490caa05
Ibrahim-Ragab <Abrahammoustafa@hotmail.co.uk>
2019-07-03 18:21:49 +0200

    Add manifest and desc

--- js/serviceworker.js
+++ js/serviceworker.js
@@ -2,7 +2,6 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('./service-worker.js', {scope: './'})
         .then((registration) => {
-            console.log(`Service Worker register ${registration}`)
         })
         .catch((err) =>{
             console.log(`Service Worker faild to register ${err}`);
